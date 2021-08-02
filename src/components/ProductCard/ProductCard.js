7e237fbf543ec08616ef69211f743208ca4b51ad
Ankush Thakur <atpara99@gmail.com>
2021-08-02 19:29:41 +0530

    Link added

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 const ProductCard = ({
   imageUrl,
   category = "NA",
@@ -9,44 +11,46 @@ const ProductCard = ({
   return (
     <a id="product-card" href="/product" className="btn text-start rounded-3">
       <div className="card mt-1" style={{ border: "none" }}>
-        <img src={imageUrl} className="card-img-top" alt="product" />
-        <div className="card-body p-1 ps-2">
-          <p
-            className="card-text my-1 card-category"
-            style={{
-              color: "#929292 !important",
-              fontSize: "11px",
+        <Link to="/product">
+          <img src={imageUrl} className="card-img-top" alt="product" />
+          <div className="card-body p-1 ps-2">
+            <p
+              className="card-text my-1 card-category"
+              style={{
+                color: "#929292 !important",
+                fontSize: "11px",
 
-              letterSpacing: "0.32px",
-              fontWeight: "500",
-            }}
-          >
-            <small className="text-muted">{category.toUpperCase()}</small>
-          </p>
-          <h5
-            className="card-title my-1 card-title"
-            style={{
-              fontWeight: 600,
+                letterSpacing: "0.32px",
+                fontWeight: "500",
+              }}
+            >
+              <small className="text-muted">{category.toUpperCase()}</small>
+            </p>
+            <h5
+              className="card-title my-1 card-title"
+              style={{
+                fontWeight: 600,
 
-              fontSize: "13px",
-              letterSpacing: "0.58px",
-              color: "#384355 !important",
-              textTransform: "capitalize",
-            }}
-          >
-            {title}
-          </h5>
-          <p
-            className="card-text my-1 card-price"
-            style={{
-              letterSpacing: "0.58px",
-              color: "#000000",
-              fontWeight: "500",
-            }}
-          >
-            ₹ {price}
-          </p>
-        </div>
+                fontSize: "13px",
+                letterSpacing: "0.58px",
+                color: "#384355 !important",
+                textTransform: "capitalize",
+              }}
+            >
+              {title}
+            </h5>
+            <p
+              className="card-text my-1 card-price"
+              style={{
+                letterSpacing: "0.58px",
+                color: "#000000",
+                fontWeight: "500",
+              }}
+            >
+              ₹ {price}
+            </p>
+          </div>
+        </Link>
       </div>
     </a>
   );
