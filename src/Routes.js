7ac6df4c1601086d0eae72e03649a45be4ec4597
Ankush Thakur <atpara99@gmail.com>
2021-08-02 13:38:45 +0530

    Hash router

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route, HashRouter as Router } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Product from "./pages/Product";
